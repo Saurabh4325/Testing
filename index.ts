0450863d96c17d5256b163e499391823cef237e5
suraj vishwakarma <smartcraze354@gmail.com>
2025-08-26 19:25:24 +0530

    init projects

---
+++ index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+});
+
+app.get("/api/data", (req, res) => {
+    res.json({ message: "This is some data from the server." });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+}
+);
