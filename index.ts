fe7d5f41cd293d261ea180288af1b0451bf2380f
suraj vishwakarma <smartcraze354@gmail.com>
2025-08-26 19:42:07 +0530

    echo routes

--- index.ts
+++ index.ts
@@ -8,6 +8,12 @@ app.get("/", (req, res) => {
     res.send("Hello, World!");
 });
 
+app.post("/api/echo", (req, res) => {
+    res.json({ received: req.body });
+
+});
+
+
 app.get("/api/data", (req, res) => {
     res.json({ message: "This is some data from the server." });
 });
